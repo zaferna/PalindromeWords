bf2c072f6ffbdebeaf32a958a2b594f399ed37bd
zaferna <106475526+zaferna@users.noreply.github.com>
2022-06-22 22:15:30 +0300

    Add files via upload

---
+++ palindromeWords.java
@@ -0,0 +1,27 @@
+public class palindromeWords {
+
+    static boolean isPalindrome(String str){
+        int i = 0 ;
+        int j= str.length()-1;
+        while(i<j){
+
+           if( str.charAt(i) != str.charAt(j)){
+               return false;
+
+           }
+           i++;
+           j--;
+        }
+
+
+
+        return true;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("ssbcss" ));
+    }
+}
